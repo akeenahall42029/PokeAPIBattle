2e011421818b137e4486fa033529d9a48ce9de6f
Akeena Hall <akeenadhall@gmail.com>
2019-05-11 16:01:38 -0400

    working with apis
    made sure the api were pulling information

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ $(document).ready(function(){
 
 
 //starter function to pull a random pokemon from the PokeAPI
+    $("#leftButton").click(getRandomPokemonLeft)
   function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
       console.log("Left Pokemon Data: ", pokeData);
+      
+      
     });
   }
-
+  $("#rightButton").click(getRandomPokemonRight)
+  function getRandomPokemonRight() {
+    let randomNumber = Math.floor(Math.random() * 800) + 1;
+    $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+      console.log("Right Pokemon Data: ", pokeData);
+      
+    
+    });
+  }
+  
 
 
 //all code here
